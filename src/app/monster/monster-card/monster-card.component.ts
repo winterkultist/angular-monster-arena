e628bc3837c64dfa28e4a1cdefcdee6e3d6f2fce
Winter Stefan <stefan.winter@twinformatics.at>
2025-02-03 10:39:04 +0100

    attack removed again

--- src/app/monster/monster-card/monster-card.component.ts
+++ src/app/monster/monster-card/monster-card.component.ts
@@ -1,12 +1,9 @@
 import {Component, Input} from '@angular/core';
 import {Monster} from '../../model/monster.model';
-import {AttackComponent} from '../attack/attack.component';
 
 @Component({
   selector: 'app-monster-card',
-  imports: [
-    AttackComponent
-  ],
+  imports: [],
   templateUrl: './monster-card.component.html',
   styleUrl: './monster-card.component.css'
 })
